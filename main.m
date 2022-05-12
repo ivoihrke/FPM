8a1c703249ce07f529e05643f91417e3bd02ba50
meshreki <john.meshreki@gmail.com>
2022-05-12 12:43:03 +0200

    Postprocess of white pixels w/ high intensity, save to matflies, add semicolons to suppress output in command.

--- main.m
+++ main.m
@@ -20,7 +20,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % use today's date to create new output directory
-todaysdate = string(datetime('now','Format','dd_MM_yyyy_HH_mm_ss'));
+todaysdatetime = string(datetime('now','Format','dd_MM_yyyy_HH_mm_ss'));
+todaysdate = string(datetime('today','Format','dd_MM_yyyy'));
+
 
 
 %% Reconstruction library locates here
@@ -45,9 +47,9 @@ sample_name= 'USAF'; %'staind', 'USAF', 'hela'
 input_dir_name = containers.Map({'stained'; 'USAF'; 'hela'},...
 {'../data/Tian14/1LED/tif/';...
    '../data/Tian14_ResTarget/1LED/';...
-   '../data/Tian15_inVitroHeLa/data/'})
+   '../data/Tian15_inVitroHeLa/data/'});
 
-filedir = input_dir_name(sample_name)
+filedir = input_dir_name(sample_name);
 
 % Generate the image list, in 'tif' image format (depending on your image format)
 imglist = dir([filedir,'*.tif']);
@@ -59,15 +61,15 @@ N = natsortfiles({imglist.name});%sorting the images in
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % map container of output directory path to
 out_dir_name = containers.Map({'stained'; 'USAF'; 'hela'},...
-{strcat('../out_dir/Tian14_StainedHistologySlide/',todaysdate,'/');...
-    strcat('../out_dir/Tian14_ResTarget/',todaysdate,'/');...
-    strcat('../out_dir/Out_Tian15_inVitroHeLa/',todaysdate,'/')})
+{strcat('../out_dir/Tian14_StainedHistologySlide/',todaysdate,'/',todaysdatetime,'/');...
+    strcat('../out_dir/Tian14_ResTarget/',todaysdate,'/',todaysdatetime,'/');...
+    strcat('../out_dir/Out_Tian15_inVitroHeLa/',todaysdate,'/',todaysdatetime,'/')});
 
-out_dir = out_dir_name(sample_name)
+out_dir = out_dir_name(sample_name);
 mkdir(out_dir);
 
 % keep a log
-diary(strcat(out_dir,'/','log_',todaysdate,'.txt'));
+diary(strcat(out_dir,'/','log_',todaysdatetime,'.txt'));
 
 
 %% define # of LEDs used to capture each image
@@ -116,14 +118,14 @@ toc;
 
 
 f_test = figure('visible','off');imshow(Iall(:,:,128));
-title('img of Iall, index==128')
+title('img of Iall, index==128');
 export_fig(f_test,strcat(out_dir,'Iall_index_128.png'),'-m4');
 
 %% define processing ROI
-%Np = 344;
+Np = 344;
 %Np = 1000;
 %Np = 2048;
-Np = 2160;
+%Np = 2160;
 
 %nstart = [1,1];
 
@@ -135,14 +137,14 @@ else
 end
 
 % overwrite nstart in USAF_Parameter.m
-nstart = [1,1];
+%nstart = [1,1];
 
 %% load in data: read in the patch from the memory
 Imea = double(Iall(nstart(1):nstart(1)+Np-1,nstart(2):nstart(2)+Np-1,:)); % why arrray 344x344x293??
 %Imea = double(Iall(1:n1-1,1:n2-1,:)); % why arrray 344x344x293??
 
 f_test = figure('visible','off');imshow(uint16(Imea(:,:,128)));
-title('img of Imea, index==128; patched into 2048x2048')
+title('img of Imea, index==128; patched into 2048x2048');
 export_fig(f_test,strcat(out_dir,'Imea_index_128.png'),'-m4');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -191,27 +193,27 @@ Ns(:,:,2) = Nsh_lit;
 f_image_illum_plane = figure('visible','off'); 
 pcolor(LitCoord);
 %labelpoints(LitCoord, string(illumination_na_used), 'FontSize', 5);
-title(['Images pattern indexed by LEDs indicies at illumination plane for all 32x32 LEDs/n with their NAs'])
+title(['Images pattern indexed by LEDs indicies at illumination plane for all 32x32 LEDs/n with their NAs']);
 export_fig(f_image_illum_plane,strcat(out_dir,'image_indexed_pattern_illum_plane.png'),'-m4');
 
 
 %Dirac centers of the 32x32 LEDs
 f_dirac_32 = figure('visible','off'); scatter(idx_u, idx_v);
-title(['Dirac peaks centers at O plane for all 32x32 LEDs'])
+title(['Dirac peaks centers at O plane for all 32x32 LEDs']);
 export_fig(f_dirac_32,strcat(out_dir,'dirac_centers_32_32.png'),'-m4');
 
 %Dirac centers of 293 LEDs sorted according to their NA
 f_dirac_293= figure('visible','off'); 
-scatter(Nsh_lit, Nsv_lit)
+scatter(Nsh_lit, Nsv_lit);
 labelpoints(Nsh_lit, Nsv_lit, string(idx_led), 'FontSize', 5);
-title(['Dirac peaks centers at O plane for all 293 LEDs after NA sorting'])
+title(['Dirac peaks centers at O plane for all 293 LEDs after NA sorting']);
 export_fig(f_dirac_293,strcat(out_dir,'dirac_centers_293_NA_sorted.png'),'-m4');
 
 Imea_reorder = Imea(:,:,idx_led);
 Ibk_reorder = Ibk(idx_led);
 
 f_test = figure('visible','off');imshow(uint16(Imea_reorder(:,:,5)));
-title('img of Imea_reordered, old_index==128, reordered_index==5; patched into 344x344')
+title('img of Imea_reordered, old_index==128, reordered_index==5; patched into 344x344');
 export_fig(f_test,strcat(out_dir,'Imea_reodered_index_128.png'),'-m4');
 
 % gain = a(idx_led);
@@ -231,7 +233,7 @@ for m = 1:Nimg
 end
 
 f_test = figure('visible','off');imshow(uint16(Ithresh_reorder(:,:,5)));
-title('img of Ithresh_reorder, old_index==128, reordered_index==5; patched into 344x344')
+title('img of Ithresh_reorder, old_index==128, reordered_index==5; patched into 344x344');
 export_fig(f_test,strcat(out_dir,'Ithresh_reorder_index_128.png'),'-m4');
 
 %load('C:\Users\Muneeb\Desktop\ajmal fpm\laura\multiplexed fpm\Ns_cal289.mat\');
@@ -247,7 +249,7 @@ Ns_reorder = Ns(:,idx_led,:); %why?
                               % illumination_na_used(147)=0.0012,
                               % dis_lit2(1)=0.0012
                               % Therefore, for each NA-index, store indicies of the corresponding k_u and k_v
-clear Imea
+clear Imea;
 %% reconstruction algorithm
 % select the index of images that will be used in the processing
 Nused = 293;
@@ -257,7 +259,7 @@ Ns2 = Ns_reorder(:,idx_used,:);
 
 
 f_test = figure('visible','off');imshow(uint16(Ithresh_reorder(:,:,5)));
-title('img of I_input_to_AlterMin, old_index==128, reordered_index==5; patched into 344x344')
+title('img of I_input_to_AlterMin, old_index==128, reordered_index==5; patched into 344x344');
 export_fig(f_test,strcat(out_dir,'I_input_to_AlterMin_index_128.png'),'-m4');
 
 %% reconstruction algorithm options: opts
@@ -287,15 +289,15 @@ export_fig(f_test,strcat(out_dir,'I_input_to_AlterMin_index_128.png'),'-m4');
 opts.tol = 1;
 opts.maxIter = 10; 
 %opts.maxIter = 20;
-%opts.minIter = 2;
+opts.minIter = 2;
 %opts.minIter = 20;
-opts.minIter = 10;
+%opts.minIter = 10;
 opts.monotone = 1;
 % 'full', display every subroutin,
 % 'iter', display only results from outer loop
 % 0, no display
 opts.display = 'full';%0;%'iter';
-opts.out_dir = out_dir
+opts.out_dir = out_dir;
 
 upsamp = @(x) padarray(x,[(N_obj-Np)/2,(N_obj-Np)/2]);
 opts.O0 = F(sqrt(I(:,:,1)));
@@ -314,7 +316,7 @@ opts.F = F;
 opts.Ft = Ft;
 opts.StepSize = 0.1;
 opts.saveIterResult = 1;
-disp(opts)
+disp(opts);
 disp(['nstart: ', num2str(nstart)]);
 disp(['Np: ', num2str(Np)]);
 f88 = [];
@@ -327,24 +329,43 @@ f88 = [];
 
 %f1 = figure; imagesc(-angle(O),[-.6,1]); axis image; colormap gray; axis off 
 
-I = mat2gray(real(O));
-f1 = figure('visible','off');imshow(I);
-title(['(', opts.mode, ')',' (O)'])
+%I = mat2gray(real(O));
+real_O = mat2gray(real(O));
+f1 = figure('visible','off');imshow(real_O);
+title(['(', opts.mode, ')',' (O)']);
 export_fig(f1,strcat(out_dir,'O_',opts.mode,'.png'),'-m4');
 
-f2 = figure('visible','off');imshow(angle(O),[]);
-title(['(', opts.mode, ')',' angle (O)'])
+angle_O = angle(O);
+f2 = figure('visible','off');imshow(angle_O,[]);
+title(['(', opts.mode, ')',' angle (O)']);
 export_fig(f2,strcat(out_dir,'angle_O_',opts.mode,'.png'),'-m4');
 
-f3 = figure('visible','off');imshow(abs(O),[])
-title(['(', opts.mode, ')',' abs (O)'])
+abs_O = abs(O);
+f3 = figure('visible','off');imshow(abs_O,[]);
+title(['(', opts.mode, ')',' abs (O)']);
 export_fig(f3,strcat(out_dir,'abs_O_',opts.mode,'.png'),'-m4');
 
-f4 = figure('visible','off'); h=barh(err_pc)
-title(['(', opts.mode, ')', ' err/iter'])
-xlabel('err')
-ylabel('iter')
+f4 = figure('visible','off'); h=barh(err_pc);
+title(['(', opts.mode, ')', ' err/iter']);
+xlabel('err');
+ylabel('iter');
 export_fig(f4,strcat(out_dir,'err_',opts.mode,'.png'),'-m4');
 
 % figure(3);imagesc(-angle(O));colormap gray;
+
+% saving variables to matlab files
+hig_res_O_matfile = fullfile(out_dir, 'hig_res_O.mat');
+save(hig_res_O_matfile, 'O', 'real_O');
+all_vars_matfile = fullfile(out_dir, 'all_vars.mat');
+save(all_vars_matfile);
+
+
+%post-processing of the white pixels w/ high intensity
+proc_abs_O = abs(O);
+proc_abs_O(abs(O)>25) = 25;
+f5 = figure('visible','off');imshow(proc_abs_O,[]);
+title(['(', opts.mode, ')',' processed abs (O)']);
+export_fig(f5,strcat(out_dir,'proc_abs_O_',opts.mode,'.png'),'-m4');
+
+
 fprintf('processing completes\n');
