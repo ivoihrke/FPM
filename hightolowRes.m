e7ecf23f73ef446bf10154cfa0c7211d10ac7b86
meshreki <john.meshreki@gmail.com>
2022-06-01 12:08:25 +0200

    Clean code.

--- hightolowRes.m
+++ hightolowRes.m
@@ -1,3 +1,13 @@
+%% helper functions
+%Fourier and inverse Transforms
+F = @(x) fftshift(fft2(x));
+Ft = @(x) fftshift(ifft2(ifftshift(x)));
+
+% operator to crop region of O
+downsamp = @(x,cen,Np) x(cen(1)-floor(Np(1)/2):cen(1)-floor(Np(1)/2)+Np(1)-1,...
+    cen(2)-floor(Np(2)/2):cen(2)-floor(Np(2)/2)+Np(2)-1);
+
+%% Specify dependencies and Date/time tags
 % use today's date to create new output directory
 todaysdatetime = string(datetime('now','Format','dd_MM_yyyy_HH_mm_ss'));
 todaysdate = string(datetime('today','Format','dd_MM_yyyy'));
@@ -14,8 +24,7 @@ addpath('FP_Func/');
 %% specify which sample to use as input
 sample_name= 'USAF'; %'stained', 'USAF', 'hela'
 
-
-%% map container of output directory path to
+%% map container of output directory path
 out_dir_name = containers.Map({'stained'; 'USAF'; 'hela'},...
 {strcat('../out_dir/Tian14_StainedHistologySlide/',todaysdate,'/',todaysdatetime,'/');...
     strcat('../out_dir/Tian14_ResTarget/',todaysdate,'/',todaysdatetime,'/');...
@@ -24,10 +33,8 @@ out_dir_name = containers.Map({'stained'; 'USAF'; 'hela'},...
 out_dir = out_dir_name(sample_name);
 mkdir(out_dir);
 
-
 % keep a log
 diary(strcat(out_dir,'/','log_',todaysdatetime,'.txt'));
-
 
 %% read in all images into the memory first
 fprintf(['loading the high resolution image...\n']);
@@ -44,10 +51,10 @@ high_res_filedir = high_res_input_dir_name(sample_name);
 load([high_res_filedir, 'output_high_res.mat']);
 
 
-% all image data
+% high res complex field
 hig_res_O = O; 
 
-% remove the white pixels from the unprocessed O
+%% remove the white pixels from the unprocessed O
 nrows = size(O, 1);
 ncols = size(O, 2);
 
@@ -61,38 +68,27 @@ end
 
 toc;
 
-% saving the high res image for testing everything is loaded correctly
+%% saving the high res image for testing everything is loaded correctly
 imwrite(uint16(hig_res_O), strcat(out_dir,'high_res_O_image.tif'));
 f1 = figure('visible','off');imshow(hig_res_O,[]);
 title('(high res O)');
 export_fig(f1,strcat(out_dir,'high_res_O_figure.tif'),'-m4');
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% apply a circle filter on the high res
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Fourier and inverse Transforms
-F = @(x) fftshift(fft2(x));
-Ft = @(x) fftshift(ifft2(ifftshift(x)));
-
-% go to Fourier space
+%% go to Fourier space
 hig_res_O_fourier = Ft(hig_res_O);
 
 
-% saving the high res image in Fourier space
-%imwrite(hig_res_O_fourier, strcat(out_dir,'high_res_O_fourier_image.png'));
-%f1 = figure('visible','off');imshow(hig_res_O_fourier);
-%title('(high res fourier O)');
-%export_fig(f1,strcat(out_dir,'high_res_O_fourier_figure.png'),'-m4');
-
-%% define ROI, dirac positions and read pupil
+%% define ROI, read pupil, and no. of images
 Np = [2160, 2560];
 %W_Na = w_NA;
+
 pupil = P;
 
-%% Get dirac peaks positions
 % No. images
 Nimg=293;
+
+%% Estimate low res from high res
 
 % all intensities for estimated low res
 I_all_est_low_res = zeros(Np(1),Np(2),Nimg);
@@ -117,7 +113,6 @@ for m = 1:Nimg
     low_res_imglist = dir([low_res_filedir,'Iled_',int2str(m),'.tif']);
 
     fn = [low_res_filedir,low_res_imglist.name];
-    %disp(fn);
 
     % read low res image
     I_meas_low_res = double(imread(fn));
@@ -125,21 +120,18 @@ for m = 1:Nimg
     % save all measured low res intensities in one array
     I_all_meas_low_res(:,:,m) = I_meas_low_res;
 
+    % find Dirac peak position for image m (1 LED per image)
     index_m = find(idx_led==m);
     dirac_cen_pos = dirac_cen(index_m, :);
+    
 
-    %% operator to crop region of O
-    downsamp = @(x,cen) x(dirac_cen_pos(1)-floor(Np(1)/2):dirac_cen_pos(1)-floor(Np(1)/2)+Np(1)-1,...
-        dirac_cen_pos(2)-floor(Np(2)/2):dirac_cen_pos(2)-floor(Np(2)/2)+Np(2)-1);
-
-    % cropy and apply the circular filter(W_Na) at the dirac peak position 
-    O_cropped = downsamp(hig_res_O_fourier,dirac_cen_pos).*P;
-    %O_cropped = hig_res_O_fourier,dirac_cen_pos.*P;
+    % crope and apply the circular filter(P) at the dirac peak position 
+    O_cropped = downsamp(hig_res_O_fourier,dirac_cen_pos,Np).*P;
 
     % go to real space
     O_est_low_res = F(O_cropped);
     
-    % save all estimated low res object complex fields in one array
+    % save all estimated low res Object complex fields in one array
     O_all_est_low_res(:,:,m) = O_est_low_res;
 
     % get estimated low res intensity from Object
@@ -148,21 +140,14 @@ for m = 1:Nimg
     % save all estimated low res intensities in one array
     I_all_est_low_res(:,:,m) = I_est_low_res;
 
-    % saving the estimated low res
+    % saving the estimated low res intensities
     imwrite(uint16(I_est_low_res), strcat(out_dir,['I_est_low_res_',int2str(m),'_image.tif']));
-    %f_I_est_low_res_147 = figure('visible','off');imshow(I_est_low_res_147, []);
-    %title('(estimated low res I 147)');
-    %export_fig(f_I_est_low_res_147,strcat(out_dir,'I_est_low_res_147_figure.tif'),'-m4');
 
-    % saving the measured low res
+    % saving the measured low res intensities
     imwrite(uint16(I_meas_low_res), strcat(out_dir,'I_meas_low_res_',int2str(m),'_image.tif'));
-    %f_I_meas_low_res = figure('visible','off');imshow(I_meas_low_res, []);
-    %title('(measured low res I 147)');
-    %export_fig(f_I_meas_low_res,strcat(out_dir,'I_meas_low_res_figure.tif'),'-m4');
 end    
 
-
-% saving variables to matlab files (careful: huge output file, 
+%% saving variables to matlab files (careful: huge output file, 
 % best to put an if cond when adding to the arrays to select a few images!)
 %low_res_both_meas_est_I_matfile = fullfile(out_dir, ['low_res_both_meas_est_I','.mat']);
 %save(low_res_both_meas_est_I_matfile, 'I_all_est_low_res', 'I_all_meas_low_res', '-v7.3');
